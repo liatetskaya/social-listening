ccaed968ac0e68ba05293e9b8807c46730dc85b1
Yuliya <yulia@quincybuiltsoftware.com>
2013-04-07 09:36:09 -0400

    Facebook OAuth authentication

---
+++ app/controllers/clients/facebook_clients_controller.rb
@@ -0,0 +1,34 @@
+class Clients::FacebookClientsController < ApplicationController
+	def new
+		@fb_auth = fb_auth
+
+		# redirect user to facebook
+		redirect_to @fb_auth.client.authorization_uri(
+		  :scope => [:read_insights, :offline_access]
+		)
+	end
+
+	def callback
+		@fb_auth = fb_auth
+		@fb_auth.client.authorization_code = params[:code]
+		access_token = @fb_auth.client.access_token! :client_auth_body
+		@fb_auth.exchange_token! access_token
+
+		#TODO: store the @fb_auth.access_token in the model
+		Rails.logger.info @fb_auth.access_token.inspect
+
+		#TODO: move that to another controller
+		#page = FbGraph::Page.new('QuincybuiltSoftwareHq').insights(
+		#	:access_token => @fb_auth.access_token
+		#)
+		#Rails.logger.info page.inspect
+
+		redirect_to root_path
+	end
+
+	def fb_auth
+		fb_auth = FbGraph::Auth.new(ENV['fb_client_id'], ENV['fb_client_secret'])
+		fb_auth.client.redirect_uri = callback_facebook_clients_url
+		fb_auth
+	end
+end
