1bb9a6852a39a6145c6d10153dd9a3ccd21de303
Yulia <liatetskaya@gmail.com>
2013-04-08 00:16:14 -0400

    Added Insights

--- app/controllers/clients/facebook_clients_controller.rb
+++ app/controllers/clients/facebook_clients_controller.rb
@@ -28,7 +28,7 @@ class Clients::FacebookClientsController < ApplicationController
             	profile.service = "FACEBOOK"
             	profile.token = acc.access_token
                 profile.status = acc.name
-                profile.service_id = acc.identifier
+                profile.user_id = acc.identifier
                 profile.save
             end 
         end 
