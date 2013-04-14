6bf8eb1b11a782aec8e63b91832ec2a048d4343b
Yulia <liatetskaya@gmail.com>
2013-04-14 10:03:24 -0400

    Added chron schedule to search for the tweets

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -13,19 +13,5 @@ class StatController < ApplicationController
     @likes = page.insights(:access_token => access_token, :metrics => 'page_like_adds')
    
    # @likes = @insights[0].Page_fan_adds
-    tw_profiles = Profile.find(:all, :conditions => ["service = ? ", "TWITTER"])
-    tw_access_token = JSON.parse(tw_profiles[0].token)
-    tw_client = TwitterOAuth::Client.new(
-      :consumer_key => ENV['tw_client_id'],
-      :consumer_secret => ENV['tw_client_secret'],
-      :token => tw_access_token['token'],
-      :secret => tw_access_token['secret']
-    )
-
-    if tw_client.authorized?
-      search = tw_client.search('#', {:geocode => '42.353121,-71.052882,0.1km'})
-      Rails.logger.info search['results'].inspect
-    end
-
   end
 end
