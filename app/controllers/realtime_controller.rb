45854aa376d82197c09132602afd67fecf6699dd
Yulia <liatetskaya@gmail.com>
2013-04-14 20:25:43 -0400

    Remove CSRF warning

--- app/controllers/realtime_controller.rb
+++ app/controllers/realtime_controller.rb
@@ -1,4 +1,6 @@
 class RealtimeController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def foursquare
     resp = JSON.parse(params['checkin'])
     # TODO: verify push secret
